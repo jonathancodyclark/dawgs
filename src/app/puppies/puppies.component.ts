14e4ee1a50bb99d64a99655d2e2353dda4cd3399
jclark <jclark@us.ci.org>
2019-10-28 10:51:58 -0600

    added week 5 puppy pics and made contact page text vw instead of px

--- src/app/puppies/puppies.component.ts
+++ src/app/puppies/puppies.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class PuppiesComponent {
   title = 'puppies';
-  fredimages = [1, 2].map((elem, ndx) => '../assets/images/Fred' + (ndx+1).toString() + '.jpg');
-  susanimages = [1, 2].map((elem, ndx) => '../assets/images/Susan' + (ndx+1).toString() + '.jpg');
-  jeraldimages = [1, 2].map((elem, ndx) => '../assets/images/Jerald' + (ndx+1).toString() + '.jpg');
-  rustyimages = [1, 2].map((elem, ndx) => '../assets/images/Rusty' + (ndx+1).toString() + '.jpg');
-  juniorimages = [1, 2].map((elem, ndx) => '../assets/images/Junior' + (ndx+1).toString() + '.jpg');
+  fredimages = [1, 2, 3].map((elem, ndx) => '../assets/images/Fred' + (ndx+1).toString() + '.jpg');
+  susanimages = [1, 2, 3].map((elem, ndx) => '../assets/images/Susan' + (ndx+1).toString() + '.jpg');
+  jeraldimages = [1, 2, 3].map((elem, ndx) => '../assets/images/Jerald' + (ndx+1).toString() + '.jpg');
+  rustyimages = [1, 2, 3].map((elem, ndx) => '../assets/images/Rusty' + (ndx+1).toString() + '.jpg');
+  juniorimages = [1, 2, 3].map((elem, ndx) => '../assets/images/Junior' + (ndx+1).toString() + '.jpg');
 }
